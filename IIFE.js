c2e42e5a51674d433642b06319ac29f691c4f9c5
Azad <mr.saiful.azad@gmail.com>
2016-08-24 10:51:29 +0600

    Ass

---
+++ IIFE.js
@@ -0,0 +1,13 @@
+/**
+ * Created by azad on 8/24/16.
+ */
+function normalFunction() {
+    
+    console.log("This is a normal function");
+    
+}
+
+(function () {
+    console.log("This is a IIFE");
+    
+})();

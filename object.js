3737e65865a5ecb590230a92317c57f2b62e54b7
Azad <mr.saiful.azad@gmail.com>
2016-09-07 15:25:49 +0600

    Learn functional programming

--- object.js
+++ object.js
@@ -7,12 +7,25 @@ function Person() {
     console.log(this);
     this.first_name = 'Saiful ';
     this.last_name = 'Azad';
+    /**
+     * This is private funtion
+     */
+    function private_fun() {
+        console.log('Print from private funtion');
+    };
+    // public funtion
+    this.public_function= function () {
 
+        console.log('Print from public funtion');
+        private_fun();
 
+    };
+    this.full_name = function () {
+        return this.first_name + this.last_name;
+    }
 }
 
-
 var person = new Person();
-
-
 console.log(person);
+person.public_function();
+console.log(person.full_name());
